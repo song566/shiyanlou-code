cc4d875f0247955358da9fb29b9d84f612a3a294
song556 <934922594@qq.com>
2024-04-26 00:39:21 +0800

    java begin

---
+++ c-file/first1.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+ 
+int main(){
+	int i=1;
+	while(i<=10){
+		printf("ubuntu下第一次使用编写c程序\n");
+		i++;
+		}
+	return 0;	
+	}
---
+++ java-file/firstjava.java
@@ -0,0 +1,8 @@
+public class firstjava{
+	public static void main(String[] args){
+		int i=0;
+		for(;i<=10;i++){
+			System.out.println("试验java程序使用");
+		}
+	}
+}
